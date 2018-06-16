772659bacf4076389e93f84624258492a787f63f
StudentOfJS <rodlewis45@gmail.com>
2018-06-16 14:07:37 +0200

    client mutation CREATE_SURVEY

--- build/mutations/index.js
+++ build/mutations/index.js
@@ -135,9 +135,9 @@ export const CREATE_SURVEY = gql`
     $companyName: String!,
     $introPage1: String!,
     $introPage2: String!,
-    $subHeading: PagesInput!,
-    $start: PagesInput!,
-    $end: PagesInput!
+    $subHeading: PagesInput,
+    $start: PagesInput,
+    $end: PagesInput
   ){
     createSurvey(
       surveyName: $surveyName,
